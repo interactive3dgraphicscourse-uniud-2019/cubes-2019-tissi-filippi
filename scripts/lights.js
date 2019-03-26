9a480d667db1649deed702edfed457633bd73a0d
Simone Tissi <simone.tissi@gmail.com>
2019-03-26 16:09:43 +0100

    Split code in some files

---
+++ scripts/lights.js
@@ -0,0 +1,18 @@
+function setHemiLight() {
+    hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+    hemiLight.color.setHSL(0.6, 1, 0.6);
+    hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+    hemiLight.position.set(0, 500, 0);
+    scene.add(hemiLight);
+  }
+  
+function setDirectionalLight() {
+    dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.color.setHSL(0.1, 1, 0.95);
+    dirLight.position.set(-1, 1.75, 1);
+    dirLight.position.multiplyScalar(50);
+    scene.add(dirLight);
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize.width = 1024;
+    dirLight.shadow.mapSize.height = 1024;
+  }
