b3a3f21418fe38775933f00dce7dcb7b0c82aa5a
Simone Tissi <simone.tissi@gmail.com>
2019-03-26 16:24:47 +0100

    Finished to split the code

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,43 @@
 
 //FUNZIONE PER IL TERRENO - DA GUARDARE PIU' AVANTI
 function setGround(){
-    var groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
-    var groundMat = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x050505 } );
-    groundMat.color.setHSL( 0.095, 1, 0.75 );
-    var ground = new THREE.Mesh( groundGeo, groundMat );
+    let groundGeo = new THREE.PlaneBufferGeometry(10000, 10000);
+    let groundMat = new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      specular: 0x050505
+    });
+    groundMat.color.setHSL(0.095, 1, 0.75);
+  
+    let ground = new THREE.Mesh(groundGeo, groundMat);
     ground.position.y = -0.5;
-    ground.rotation.x = -Math.PI/2;
-    scene.add( ground );
+    ground.rotation.x = -Math.PI / 2;
+    scene.add(ground);
     ground.receiveShadow = true;
 }
+
+//FUNZIONE PER CUBO CON TEXTURE
+function createCube(){
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    let loader = new THREE.TextureLoader();
+    loader.load(
+        // resource URL
+        "textures/11635.jpg",
+        // onLoad callback
+        texture => {
+            let material = new THREE.MeshPhongMaterial({ map: texture });
+            let cube = new THREE.Mesh(geometry, material);
+            cube.castShadow = true;
+            cube.receiveShadow = true;
+            scene.add(cube);
+        },
+        // onProgress callback currently not supported
+        undefined,
+        // onError callback
+        err => {
+            console.error("An error happened.");
+            console.error(err);
+        }
+    );
+}
+
 			
