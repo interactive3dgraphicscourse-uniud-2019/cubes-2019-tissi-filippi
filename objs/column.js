e3a2cad71571abcf6bad46125a020cabdfa85b64
Davide  Filippi <davide@MacBook-Pro-di-Davide.local>
2019-03-27 16:52:44 +0100

    Colonna al fusto

--- objs/column.js
+++ objs/column.js
@@ -8,17 +8,17 @@ class Column {
 
       let base0 = this.createPartOf(columnG, columnM, 15, 5);
       let base1 = this.createPartOf(columnG, columnM, 10, 3);
-      let stem = this.createPartOf(columnG,columnM, 6, 50);
+      let stem = this.createPartOf(columnG,columnM, 6, 59);
       let capital = this.createPartOf(columnG,columnM, 10, 3); 
 
       this.column.add(base0);
-      base0.position.set(0, 20,0);
+      base0.position.y = 17.5;
       this.column.add(base1);
-      base1.position.y = 30;
+      base1.position.y = 21.5;
       this.column.add(stem);
-      stem.position.y = 35;
+      stem.position.y = 52.5; 
       this.column.add(capital);
-      capital.position.y = 85;
+      capital.position.y = 100;
     }
   
     getColumn(){
