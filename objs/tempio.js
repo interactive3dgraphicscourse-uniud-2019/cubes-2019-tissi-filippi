89c76f6f84ce8b138f1718dccabdbb8d0a34da17
Simone Tissi <simone.tissi@gmail.com>
2019-03-27 11:03:29 +0100

    Test on steps

--- objs/tempio.js
+++ objs/tempio.js
@@ -1,57 +1,21 @@
-function Tempio (x=0, y=0, z=0){
-	//this.geometry = new THREE.BoxGeometry(1,1,1);
+class Steps {
 
-	this.steps = new THREE.Object3D();
+    constructor(x,y,z) {
+      this.steps = new THREE.Object3D();
+      let step_tex = new THREE.TextureLoader().load('textures/stone.jpg');
+	  var stepM = new THREE.MeshPhongMaterial( { map: step_tex } );
 
-	// Primo gradino 
-	let step0 = this.createStep(10, 20, 3);
-	this.steps.add(step0);
-    step0.position.set(x,y,z);
+      var stepG = new THREE.BoxGeometry(1,1,1);
+      var step0 = new THREE.Mesh(stepG, stepM);
+      step0.castShadow = true;
+      step0.receiveShadow = true;
+
+      step0.position.set(x,y,z);
+      this.steps.add(step0);
+    }
+  
     
-    // Secondo gradino
-    let step1 = this.createStep(10, 20, 3);
-	this.steps.add(step1);
-    step1.position.set(x,y+3,z);
-
-    /*
-	col = [];
-	for (let i = 0; i < 4; i++) {
-		col[i] = new Column();
-		this.floor.add(col[i].col);
-		col[i].col.scale.set(0.44, 0.44, 0.44);
-	}
-
-	col[0].col.position.set(15.5,1,15.5);
-	col[1].col.position.set(-15.5,1,15.5);
-	col[2].col.position.set(15.5,1,-15.5);
-    col[3].col.position.set(-15.5,1,-15.5);
-    */
-
-}
-
-
-Tempio.prototype.createStep = function(x,y,z){
-	let geometry = new THREE.BoxGeometry(x, y, z);
-    let loader = new THREE.TextureLoader();
-    let cube;
-    loader.load(
-        // resource URL
-        "textures/stone.jpg",
-        // onLoad callback
-        texture => {
-            let material = new THREE.MeshPhongMaterial({ map: texture });
-            cube = new THREE.Mesh(geometry, material);
-            cube.castShadow = true;
-            cube.receiveShadow = true;
-        },
-        // onProgress callback currently not supported
-        undefined,
-        // onError callback
-        err => {
-            console.error("An error happened.");
-            console.error(err);
-        }
-    );
-
-	return cube;
-}
+    getSteps(){
+        return this.steps;
+      }
+  }
