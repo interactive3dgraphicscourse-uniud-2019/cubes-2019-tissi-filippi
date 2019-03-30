60e1821d787cd94c9c11698703b5957e350bf430
Davide  Filippi <davide@MacBook-Pro-di-Davide.local>
2019-03-30 13:51:57 +0100

    Aggiunte tutte le decorazioni

--- objs/decoration.js
+++ objs/decoration.js
@@ -7,14 +7,8 @@ class Decorations {
       let decorationG = new THREE.BoxGeometry(1,1,1);
 
       let dec0 = this.createDecoration(decorationG, decorationM, size, deph);
-      let dec1 = this.createDecoration(decorationG, decorationM, size, deph);
-      
-      //Aggiungo e posiziono i gradini
       this.decorations.add(dec0);
-      dec0.position.set(0,86+(8/2),79+(8/2));
-      //this.decorations.add(dec1);
-      //dec1.position.set(-45-15,86+(8/2),0);
-      //dec1.rotation.y = 90 * Math.PI/180;
+      dec0.position.set(0,0,0);
     }
   
     getDecorations(){
