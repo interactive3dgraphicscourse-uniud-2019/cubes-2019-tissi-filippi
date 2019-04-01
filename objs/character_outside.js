731afba14284276b648812a2c63ba183e3295388
Simone Tissi <simone.tissi@gmail.com>
2019-04-01 10:09:43 +0200

    Edit decorations and character inside

--- objs/character_outside.js
+++ objs/character_outside.js
@@ -3,58 +3,61 @@ class CharacterOutside {
     constructor() {
       this.character = new THREE.Object3D();
       let character_tex = new THREE.TextureLoader().load('textures/ros.jpg');
-      //let stepM = new THREE.MeshPhongMaterial( { map: character_tex } );
-      //Geometria comune: viene utilizzata per tutti gli elementi, che poi vengono scalati
-      //let stepG = new THREE.BoxGeometry(1,1,1);
       
       /****************** Creo il bottom *************/
       let bottom = [];
+      let h_level = 2.5;
+      let h_level_2 = 2;
       let bottom_character = new THREE.Object3D();
       let bottomG = new THREE.BoxGeometry(1,1,1);
       let bottomM = new THREE.MeshPhongMaterial( { map: character_tex, opacity: .8, transparent: true } );
-      bottom.push(this.createBottomLevel(bottomG, bottomM, 1.5, 2, 2)); //livello 0
+      bottom.push(this.createBottomLevel(bottomG, bottomM, 1.5, h_level_2, 2)); //livello 0
       bottom_character.add(bottom[0]);
-      bottom[0].position.set(0, -(2/2 + 2.5 +2), -(2/2 + 2 + 1))
+      bottom[0].position.set(0, -(h_level_2/2 + h_level +h_level_2), -(h_level_2/2 + h_level_2 + 1))
 
-      bottom.push(this.createBottomLevel(bottomG, bottomM, 3, 2, 2)); //livello 1
+      bottom.push(this.createBottomLevel(bottomG, bottomM, 3,h_level_2, h_level_2)); //livello 1
       bottom_character.add(bottom[1]);
-      bottom[1].position.set(0, -(2/2 +2.5), -(2/2 + 2))
+      bottom[1].position.set(0, -(h_level_2/2 + h_level), -(h_level_2/2 + h_level_2))
 
-      bottom.push(this.createBottomLevel(bottomG, bottomM, 5, 2.5, 2)); //livello 2
+      bottom.push(this.createBottomLevel(bottomG, bottomM, 5, h_level, 2)); //livello 2
       bottom_character.add(bottom[2]);
-      bottom[2].position.set(0,-2.5/2 , -2)
+      bottom[2].position.set(0,-h_level/2 , -h_level_2)
 
-      bottom.push(this.createBottomLevel(bottomG, bottomM, 7, 2.5, 4)); //livello 3
+      bottom.push(this.createBottomLevel(bottomG, bottomM, 7, h_level, 4)); //livello 3
       bottom_character.add(bottom[3]);
-      bottom[3].position.set(0,2.5/2,0)
+      bottom[3].position.set(0,h_level/2,0)
 
       /****************** Creo l'upper *************/
       let upper_character = new THREE.Object3D();
       
       //Busto
-      let bodyG = new THREE.BoxGeometry(7,10,4);
+      let w_body = 10;
+      let h_chest = 10;
+      let w_body_in = 7;
+      let w_arm_hand = 1.5;
+      let bodyG = new THREE.BoxGeometry(w_body_in, h_chest,4);
       let bodyM = new THREE.MeshBasicMaterial( { color: "rgb(0,0,0)", opacity: .8, transparent: true } );
       let body = new THREE.Mesh(bodyG, bodyM);
       upper_character.add(body);
-      body.position.set(0, 10/2 + 2.5, 0);
+      body.position.set(0, w_body/2 + h_level, 0);
 
       //Braccia e mani
-      let armG = new THREE.BoxGeometry(1.5, 5, 2);
+      let armG = new THREE.BoxGeometry(w_arm_hand, 5, 2);
       let armM = new THREE.MeshBasicMaterial( { color: "rgb(0,0,0)", opacity: .8, transparent: true } );
-      let handG = new THREE.BoxGeometry(1.5, 2, 2);
+      let handG = new THREE.BoxGeometry(w_arm_hand, 2, 2);
       let handM = new THREE.MeshBasicMaterial( { color: "rgb(255,222,173)", opacity: .8, transparent: true } );
       let armSx = new THREE.Mesh(armG, armM);
       let armDx = new THREE.Mesh(armG, armM);
       let handSx = new THREE.Mesh(handG, handM);
       let handDx = new THREE.Mesh(handG, handM);
       upper_character.add(armSx);
-      armSx.position.set(-(7/2+1.5/2), 5/2 + 2.5 + 10/2, 0);
+      armSx.position.set(-(w_body_in/2+w_arm_hand/2), 5/2 + 2.5 + h_chest/2, 0);
       upper_character.add(armDx);
-      armDx.position.set(7/2+1.5/2, 5/2 + 2.5 + 10/2, 0);
+      armDx.position.set(w_body_in/2+w_arm_hand/2, 5/2 + 2.5 + h_chest/2, 0);
       upper_character.add(handSx);
-      handSx.position.set(-(7/2+1.5/2), 2/2 + 2.5 + 3, 0);
+      handSx.position.set(-(w_body_in/2+w_arm_hand/2), 2/2 + 2.5 + 3, 0);
       upper_character.add(handDx);
-      handDx.position.set(7/2+1.5/2, 2/2 + 2.5 + 3, 0);
+      handDx.position.set(w_body_in/2+w_arm_hand/2, 2/2 + 2.5 + 3, 0);
       
       /************ Creo la testa **********************/
         let head_tex = new THREE.TextureLoader().load('textures/head_tex.jpg');
@@ -72,7 +75,7 @@ class CharacterOutside {
       let headM = new THREE.MultiMaterial(headM_array);
       let head = new THREE.Mesh(headG, headM);
       upper_character.add(head);
-      head.position.set(0, 3/2 + 2.5 + 10, 0);
+      head.position.set(0, 3/2 + 2.5 + h_chest, 0);
       
       this.character.add(bottom_character);
       this.character.add(upper_character);
